d6166f0bb4b1e052065a22cde429761b7503d109
tarika <arnaut.tarik@nsoft.com>
2025-08-24 02:02:30 +0200

    Git action

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
+// vite.config.ts
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
